cb8ae6d65fb23ed4e7f66395f9b7f9678312c285
Ángel De La Cruz <8610043+angeldelacruzdev@users.noreply.github.com>
2022-02-08 19:04:14 -0400

    Suma, división y multiplicación de números.

--- Practica_Metodos/Program.cs
+++ Practica_Metodos/Program.cs
@@ -5,13 +5,33 @@ namespace Practica_Metodos
     {
         static void Main(string[] args)
         {
-            SumaNumeros(5,6);
 
+            Console.WriteLine("Suma de números");
+
+            Console.WriteLine(SumaNumeros(5, 6));
+            
+            Console.WriteLine("Dividiendo números");
+
+            Console.WriteLine(DividiendoNumeros(5, 2));
+
+            Console.WriteLine(MultiplicaNumeros(5, 2));
         }
 
-       static void SumaNumeros(int num1, int num2)
+        //Retorna la suma de dos valores
+       static int SumaNumeros(int num1, int num2)
         {
-            Console.WriteLine("La suma de los números es: " + (num1 + num2));
+           return num1 + num2;
+        }
+
+        //Retorna dos números divididos
+        static double DividiendoNumeros(double num1, int num2) => num1 / num2;
+
+        //Retorna dos números multiplicados
+        static int MultiplicaNumeros(int num1, int num2)
+        {
+            int resultado;
+            resultado = num1 * num2;
+            return resultado;
         }
     }
 }
