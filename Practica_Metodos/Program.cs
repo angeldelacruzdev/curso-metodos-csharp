94222de694643daae1adcdf8263826fbcff13339
Ángel De La Cruz <8610043+angeldelacruzdev@users.noreply.github.com>
2022-02-08 18:27:51 -0400

    first commit method

---
+++ Practica_Metodos/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Practica_Metodos 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mensajeEnPantalla();
+
+            Console.WriteLine("Mensaje desde el main");
+        }
+
+        static void mensajeEnPantalla()
+        {
+            Console.WriteLine("Mensaje desde el método mensajeEnPatalla");
+        }
+    }
+}
