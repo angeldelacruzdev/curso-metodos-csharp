8481da234debb10e77be99ea415bc823526a3a87
Ángel De La Cruz <8610043+angeldelacruzdev@users.noreply.github.com>
2022-02-09 08:28:46 -0400

    Curso C#. Excepciones I. Vídeo 22

--- Practica_Metodos/Program.cs
+++ Practica_Metodos/Program.cs
@@ -6,11 +6,42 @@ namespace Practica_Metodos
        
         static void Main(string[] args)
         {
-            Console.WriteLine(Suma(5, 6, 22));
+            Random numero = new();
+            
+            int aleatorio = numero.Next(0, 100);
+
+            int mi_numero;
+
+            int intentos = 0;
+
+            Console.WriteLine("Intentos un  número entre 0 y 100");
+
+            do
+            {
+                intentos++;
+
+                try {
+                    mi_numero = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("No has introducido un valor numérico válido");
+                    mi_numero = 0;
+                }
+
+            
+
+                if (mi_numero > aleatorio) Console.WriteLine("El número es más bajo");
+
+                if (mi_numero < aleatorio) Console.WriteLine("El número es más alto");
+
+            } while (aleatorio != mi_numero);
+
+            Console.WriteLine($"Correcto! Has necesitado {intentos} intentos");
+            Console.WriteLine("Apartir de esta línea de código el programa continuara");
         }
 
-        //Parámetro opcionales
-        private static double Suma(int v1, double v2, double v3 = 0) => v1 + v2 + v3;
+ 
     }
 }
  
